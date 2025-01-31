521a23b9687a93f0b54d34031a9a46990e5633a0
acce8711 <101413675+acce8711@users.noreply.github.com>
2025-01-30 20:27:49 -0500

    listen for room onclick event to throw piece

--- public/scripts/throw-piece.js
+++ public/scripts/throw-piece.js
@@ -1,51 +1,70 @@
 AFRAME.registerComponent("throw-piece", {
     init: function(){
-        CONST 
-        const plane = document.querySelector("#wall-z-negative")
-        plane.addEventListener("click", () => {
-            throwPiece();
-           
+        const Context_AF = this;
+        Context_AF.room =  document.querySelector("#room");
+        Context_AF.furniture =  document.querySelector("#furniture");
+        
+        Context_AF.room.addEventListener("click", function(event){
+            //get the cursor intersection point
+            console.log("hello")
+            const cursorPos = event.detail.intersection.point;
+            throwPiece(cursorPos, Context_AF);
         })
+
+        Context_AF.furniture.addEventListener("click", function(event){
+            //get the cursor intersection point
+            console.log("furniture")
+            // const cursorPos = event.detail.intersection.point;
+            // throwPiece(cursorPos, Context_AF);
+        })
+
+        // Context_AF.wallZPositive.addEventListener("click", function(event){
+        //     //get the cursor intersection point
+        //     const cursorPos = event.detail.intersection.point;
+        //     throwPiece(cursorPos, Context_AF);
+        // })
+
+        // Context_AF.wallXNegative.addEventListener("click", function(event){
+        //     //get the cursor intersection point
+        //     const cursorPos = event.detail.intersection.point;
+        //     throwPiece(cursorPos, Context_AF);
+        // })
+
+        // Context_AF.wallXPositive.addEventListener("click", function(event){
+        //     //get the cursor intersection point
+        //     const cursorPos = event.detail.intersection.point;
+        //     throwPiece(cursorPos, Context_AF);
+        // })
     }
 }
 )
 
-const throwPiece = function()
+const throwPiece = function(cursorPos, Context_AF)
 {
-    console.log("throw")
     const canThrow = document.querySelector("#manager").getAttribute("manager").canThrow;
-
     if(canThrow)
     {
-        const el = this.el;
-        var worldPosition = new THREE.Vector3();
-        this.el.object3D.getWorldPosition(worldPosition);
-        //const elPos = el.object3D.position;
-    
-        console.log(el)
-        //get the cursor intersection point
-        const cursorPos = event.detail.intersection.point;
-        var worldPosition = new THREE.Vector3();
-        this.el.object3D.getWorldPosition(worldPosition);
+        const worldPosition = new THREE.Vector3();
+        Context_AF.el.object3D.getWorldPosition(worldPosition);
 
         //create a vector between the camera and the cursor position (target)
         //this vector will represent the target throw direction
         const direction = new THREE.Vector3(cursorPos.x - worldPosition.x, cursorPos.y - worldPosition.y, cursorPos.z - worldPosition.z);
 
         //add dynamic body component 
-        el.setAttribute("ammo-body", {type: "dynamic", emitCollisionEvents: true});
-        el.setAttribute("ammo-shape", {type: "sphere", fit: "manual", sphereRadius: 0.4});
+        Context_AF.el.setAttribute("ammo-body", {type: "dynamic", emitCollisionEvents: true});
+        Context_AF.el.setAttribute("ammo-shape", {type: "sphere", fit: "manual", sphereRadius: 0.4});
 
         //throw the piece in the direction of the cursor
-        const x = direction.x * 2;
-        const z = direction.z * 2;
-        const y = direction.y * 2;
+        const x = direction.x * 4;
+        const z = direction.z * 4;
+        const y = direction.y * 4;
         const force = new Ammo.btVector3(x, y, z);
         const pos = new Ammo.btVector3(worldPosition.x, worldPosition.y, worldPosition.z);
-        el.body.applyImpulse(force, pos);
+        Context_AF.el.body.applyImpulse(force, pos);
         
         //add event collision component when the pottery piece has been thrown
-        el.setAttribute("destroy-pottery", "");
+        Context_AF.el.setAttribute("destroy-pottery", "");
     }
 }
 
