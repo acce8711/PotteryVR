9458a131f49788f532811dcc39741a3c1c61180d
acce8711 <101413675+acce8711@users.noreply.github.com>
2025-01-31 02:14:01 -0500

    add UI

--- public/scripts/constants.js
+++ public/scripts/constants.js
@@ -25,3 +25,16 @@ const CAN_PICKUP = "canPickUp";
 const CAN_PLACE = "canPlace";
 const CAN_THROW = "canThrow";
 
+//instruction text
+const CLAY_INSTRUCTIONS = "Let's start off with placing some clay on the wheel 😊";
+const WHEEL_INSTRUCTIONS = "It's time to turn on the wheel!";
+const MODIFICATION_INSTRUCTIONS = "Use the sliders to shape your pot.\n(Pause the wheel when you're happy with your pot 😎)";
+const PICKUP_INSTRUCTIONS = "It's time to pick up your pot...";
+const THROW_INSTRUCTIONS = "And let it go. Double click anywhere on the wall, ceiling, or floor 😉";
+const REPEAT_INSTRUCTIONS = "Now. Repeat.";
+
+//error text
+const CLAY_ERROR = "Uh oh. You can't create a new pot yet."
+const WHEEL_ERROR = "Uh oh. You can't turn on the wheel yet."
+const PICKUP_ERROR = "Uh oh. You can't pick up the pot yet. Gotta turn off the wheel 😉"
+
